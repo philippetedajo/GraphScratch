126041d1f82e571b3da7b008608c41c09683c0b2
philippetedajo <philimart21@gmail.com>
2020-12-25 03:44:05 +0100

    add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
+import Router from "next/router";
+import NProgress from "nprogress";
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }) {
   const Template = Component.Template || EmptyTemplate;
+
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
+
   return (
     <Template>
       <Component {...pageProps} />
