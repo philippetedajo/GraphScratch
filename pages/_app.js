50d6b090577d68f258a7104b61426511d6ef3184
Philippe <Philimart21@gmail.com>
2021-02-23 13:59:58 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import { Progress } from "../components";
 import "../styles/global.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 function App({ Component, pageProps }) {
   const Template = Component.Template || EmptyTemplate;
   Progress();
 
+  const client = new ApolloClient({
+    uri: "https://graphqlzero.almansi.me/api",
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <Template>
-      <Component {...pageProps} />
-    </Template>
+    <ApolloProvider client={client}>
+      <Template>
+        <Component {...pageProps} />
+      </Template>
+    </ApolloProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,63 @@
 import Head from "next/head";
-import PageTemplate from "../templates/page.template";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { Get_All_Posts } from "../utils/constants";
 
 const Home = () => {
+  let options = {
+    paginate: {
+      page: 1,
+      limit: 5,
+    },
+  };
+  const [postsField, setPostsFields] = useState({});
+  const { loading, error, data } = useQuery(Get_All_Posts, {
+    variables: { options },
+  });
+
+  if (loading) return <div className="text-2xl">...loading</div>;
+  if (error) `Error! ${error.message}`;
+
+  const posts = data.posts.data;
+
+  const articlesList = posts.map((post, index) => (
+    <div key={index} className="border-2 rounded my-2 w-2/3">
+      {post.title}
+    </div>
+  ));
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setPostsFields({ ...postsField, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(postsField);
+  };
+
   return (
     <div>
       <Head>
         <title>Nextwind</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex justify-center">
-        <p className="mt-14 text-2xl md:text-4xl text-center w-2/3">
-          Nextwind is a complete Next Js boilerplate designed with Tailwind css,
-          allowing you to quickly start a Next js project, it currently has a an
-          authentication layout and few pages.
-        </p>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            title:
+            <input onChange={handleOnChange} type="text" name="title" />
+          </label>
+          <label>
+            body:
+            <input onChange={handleOnChange} type="text" name="body" />
+          </label>
+          <input type="submit" value="Create" />
+        </form>
       </div>
+      <div>{articlesList}</div>
     </div>
   );
 };
 
 export default Home;
-
-Home.Template = PageTemplate;
