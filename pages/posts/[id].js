e772eb2ebd2808eb2a99747aa5df429d781837c0
Philippe <Philimart21@gmail.com>
2021-02-25 18:49:08 +0100

    update

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useMutation, useQuery } from "@apollo/client";
 import { Get_Post, Update_Post } from "../../utils/constants";
 
 const Post = () => {
   const Router = useRouter();
-  const [inputField, setInputField] = useState();
+  const [inputField, setInputField] = useState({});
+  const [id, setId] = useState();
+
+  useEffect(() => {
+    setId(Router.query.id);
+  }, []);
 
   //Get a post
   const { loading, error, data } = useQuery(Get_Post, {
     variables: {
-      id: Router.query.id,
+      id: id,
     },
   });
 
@@ -20,13 +25,22 @@ const Post = () => {
   if (loading) return <div className="text-2xl">...loading</div>;
   if (error) `Error! ${error.message}`;
 
-  const { id, title, body } = data.post;
+  // setInputField({ ...inputField, data });
 
   const handleSubmit = () => {};
-  const handleOnChange = () => {};
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputField({ ...inputField, [name]: value });
+  };
+
+  console.log(inputField);
 
   return (
     <div>
+      <div className="mb-10 cursor-pointer" onClick={() => Router.back()}>
+        &#8592; back
+      </div>
       <form className="flex flex-col" onSubmit={handleSubmit}>
         <label className="mr-3 mb-5">
           title:
@@ -35,6 +49,7 @@ const Post = () => {
             onChange={handleOnChange}
             type="text"
             name="title"
+            value={inputField.title}
           />
         </label>
         <label className="mr-3 mb-5">
@@ -43,6 +58,7 @@ const Post = () => {
             className="border rounded w-96 ml-2 pl-1"
             onChange={handleOnChange}
             name="body"
+            value={inputField.body}
           />
         </label>
         <input className="w-32" type="submit" value="Update a post" />
